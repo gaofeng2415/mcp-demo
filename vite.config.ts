adf6cc86079711f7a2739cf19e432a3af814dc76
高峯 <gaofeng@gigacloudtech.com>
2025-05-07 09:30:07 +0800

    调整vite.config.ts结构，添加env.local

--- vite.config.ts
+++ vite.config.ts
@@ -3,40 +3,41 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import path from 'path'
 
-const env = loadEnv('development', process.cwd())
-
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 8080,
-    proxy: {
-      '/maas/v1': {
-        target: env.VITE_OPENAI_BASE_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/maas\/v1/, '')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
       }
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      sass: {
-        additionalData: '@import "@/client/styles/variables.scss"; @import "@/client/styles/base.scss";', // 全局sass变量文件
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 8080,
+      proxy: {
+        '/maas/v1': {
+          target: env.VITE_OPENAI_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/maas\/v1/, '')
+        }
       }
-    }
-  },
-  build: {
-    outDir: path.resolve(__dirname, './dist/client'),
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, './src/client/main.ts')
+    },
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData: '@import "@/client/styles/variables.scss"; @import "@/client/styles/base.scss";', // 全局sass变量文件
+        }
+      }
+    },
+    build: {
+      outDir: path.resolve(__dirname, './dist/client'),
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, './src/client/main.ts')
+        }
       }
     }
   }
